32c595a3cd8dfca2c780c6325bc9e94214829566
Steve Bennett <stevebennett08@gmail.com>
2015-06-08 17:08:37 -0400

    override test runner

---
+++ test/my_stupid_test.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+main() {
+  test("really dumb test", () {
+    expect("i know you are but what am i", equals("i know you are but what am i"));
+  });
+}  
--- test/test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:test/test.dart';
-
-main() {
-  test("test", () {
-    expect("test", equals("test"));
-  });
-}  
